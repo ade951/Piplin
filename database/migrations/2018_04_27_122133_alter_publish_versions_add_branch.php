fbae6245018378c7b805a9a21706efcb5aa9f8f8
zhangjianwei <zhangjianwei@youzan.com>
2018-05-01 17:13:33 +0800

    migrations文件修复

--- database/migrations/2018_04_27_122133_alter_publish_versions_add_branch.php
+++ database/migrations/2018_04_27_122133_alter_publish_versions_add_branch.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterPublishVersionsAddBranchChangeReason extends Migration
+class AlterPublishVersionsAddBranch extends Migration
 {
     private $tableName = 'publish_versions';
 
